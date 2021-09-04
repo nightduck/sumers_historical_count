5f8a5d285741a685e766a444e935a4b99312646d
oren <virtualoren@gmail.com>
2021-09-04 04:16:31 +0000

    Modified entrypoint (and made executable)

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
 python /app/app.py &
 
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-*/4 * * * * /usr/local/bin/python /app/scrape_gym.py
+*/15 * * * * /usr/local/bin/python /app/scrape_gym.py
 # This extra line makes it a valid cron" > scrape_cron.txt
 
 crontab scrape_cron.txt
